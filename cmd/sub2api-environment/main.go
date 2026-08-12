16147e25550fda1df97fb679d512b853525731a1
c_w_xiaohei <1641233466@qq.com>
2026-08-12 20:43:06 +0800

    Add Environment Program caller

---
+++ cmd/sub2api-environment/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/c-w-xiaohei/sub2api-deploy/internal/artifact"
+	"github.com/c-w-xiaohei/sub2api-deploy/internal/program"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	if err := pulumi.RunErr(run); err != nil {
+		fmt.Fprintln(os.Stderr, "environment program failed")
+		os.Exit(1)
+	}
+}
+
+func run(ctx *pulumi.Context) error {
+	executable, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("locate environment program")
+	}
+	return register(ctx, executable)
+}
+
+func register(ctx *pulumi.Context, executablePath string) error {
+	config, hasConfig := ctx.GetConfig("sub2api-environment:environmentConfig")
+	secrets, hasSecrets := ctx.GetConfig("sub2api-environment:environmentSecrets")
+	if !hasConfig || !hasSecrets || ctx.IsConfigSecret("sub2api-environment:environmentConfig") || !ctx.IsConfigSecret("sub2api-environment:environmentSecrets") {
+		return fmt.Errorf("invalid environment configuration")
+	}
+
+	bundle, err := artifact.LoadBundle(filepath.Join(filepath.Dir(filepath.Dir(executablePath)), "artifacts", "sub2api-host"))
+	if err != nil {
+		return fmt.Errorf("invalid environment artifact bundle")
+	}
+	return program.Register(ctx, bundle.Manifest.Release, []byte(config), []byte(secrets))
+}
